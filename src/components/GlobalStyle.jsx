33f203c691395ab58cc7d2ace5a7bdeeb1c49d84
aovoq <a21dc163@dhw.ac.jp>
2022-03-25 23:57:42 +0900

    ADD reset / global styles

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+   :root {
+      --accent: hsl(54, 100%, 55%);
+      --text: hsl(187, 13%, 86%);
+      --bg: hsl(227, 16%, 9%);
+      --base-font: -apple-system, BlinkMacSystem, sans-serif;
+   }
+
+   *, *::before, *::after {
+      margin: 0;
+      padding: 0;
+      box-sizing: inherit;
+   }
+
+   html, body {
+      box-sizing: border-box;
+      font-size: 62.5%;
+      overflow: hidden;
+   }
+
+   h1, h2, h3, h4, h5, h6 {
+      font-size: 100%;
+      font-weight: normal
+   }
+
+   ul {
+      list-style: none;
+   }
+
+   img, video {
+      height: auto;
+      max-width: 100%;
+   }
+
+   iframe {
+      border: 0;
+   }
+
+   table {
+      border-collapse: collapse;
+      border-spacing: 0;
+   }
+`
+
+export default GlobalStyle
