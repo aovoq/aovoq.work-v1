ebe5151e0469eca42a8b1fdc4e51585339f91667
aovoq <a21dc163@dhw.ac.jp>
2022-03-26 23:20:52 +0900

    App.jsx ^^;

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Skills from './components/Skills'
+import RouterConfig from './utils/RouterConfig'
+import styled from 'styled-components'
+
 function App() {
-   return <>init</>
+   return (
+      <Root>
+         <RouterConfig />
+      </Root>
+   )
 }
+
+const Root = styled.main`
+   overflow: hidden;
+   background-color: var(--bg);
+   width: 100vw;
+   height: 100vh;
+`
 
 export default App
