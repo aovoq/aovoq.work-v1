e7b92605c0d689af9639ce18046f7cc59e5dbef8
aovoq <a21dc163@dhw.ac.jp>
2022-04-10 00:00:00 +0900

    soon page add

---
+++ src/pages/Soon.jsx
@@ -0,0 +1,35 @@
+import { Link, useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import Header from '../components/Header'
+
+const Soon = () => {
+   const navigate = useNavigate()
+   return (
+      <Container
+         onClick={() => {
+            navigate('/')
+         }}>
+         <Header />
+         <Text>
+            <Link to='/'>このページはまだ準備中です。</Link>
+         </Text>
+      </Container>
+   )
+}
+
+const Container = styled.div`
+   position: relative;
+   width: 100%;
+   height: 100%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`
+
+const Text = styled.h1`
+   color: var(--text);
+   font-size: 1rem;
+   letter-spacing: 1.5rem;
+`
+
+export default Soon
