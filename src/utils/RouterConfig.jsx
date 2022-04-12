83034349467533fe09f38254ac5df883db5645c1
aovoq <a21dc163@dhw.ac.jp>
2022-04-13 00:00:00 +0900

    Router config add soon

--- src/utils/RouterConfig.jsx
+++ src/utils/RouterConfig.jsx
@@ -3,13 +3,17 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import Skillset from '../pages/Skillset'
 import NotFound from '../pages/NotFound'
+import Soon from '../pages/Soon'
 
 const RouterConfig = () => {
    return (
       <BrowserRouter>
          <Routes>
             <Route index element={<Home />} />
-            <Route path='about' element={<About />} />
+            {/* <Route path='about' element={<About />} /> */}
+            <Route path='about' element={<Soon />} />
+            <Route path='works' element={<Soon />} />
+            <Route path='contact' element={<Soon />} />
             <Route path='skillset' element={<Skillset />} />
             <Route path='*' element={<NotFound />} />
          </Routes>
