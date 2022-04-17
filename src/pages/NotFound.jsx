95db76f0fb640f88751d950a49cfab29b5acef50
aovoq <a21dc163@dhw.ac.jp>
2022-04-18 00:00:00 +0900

    404 page organized

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
-import { AiOutlineRollback } from 'react-icons/ai'
 import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import Header from '../components/Header'
 
 const NotFound = () => {
    const navigate = useNavigate()
@@ -9,12 +9,7 @@ const NotFound = () => {
          onClick={() => {
             navigate('/')
          }}>
-         <Back>
-            <Link to='/'>
-               <span>BACK</span>
-               <AiOutlineRollback />
-            </Link>
-         </Back>
+         <Header />
          <Text>
             <Link to='/'>なぜあなたはこのURLを踏みますか？</Link>
          </Text>
@@ -35,21 +30,6 @@ const Text = styled.h1`
    color: var(--text);
    font-size: 1rem;
    letter-spacing: 1.5rem;
-`
-
-const Back = styled.div`
-   position: absolute;
-   top: 2rem;
-   right: 3rem;
-   font-size: 2rem;
-   a {
-      display: flex;
-      align-items: center;
-   }
-   span {
-      font-size: 1rem;
-      font-weight: bold;
-   }
 `
 
 export default NotFound
